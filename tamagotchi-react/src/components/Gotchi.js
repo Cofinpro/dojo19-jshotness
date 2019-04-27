447e0c8e4dc0273270d815db207b0b1ebc460153
Anne Penning <apenning@MB018.local>
2019-04-27 16:20:37 +0200

    medicine button

--- tamagotchi-react/src/components/Gotchi.js
+++ tamagotchi-react/src/components/Gotchi.js
@@ -5,20 +5,26 @@ import sick_green from '../emojis/sick_green.png';
 import sick_thermo from '../emojis/sick_thermometer.png';
 import './Gotchi.css';
 
+
+
 class Gotchi extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       age: 0, // starting from 0
       mood: 0, // range: -5 to 5
-      health: 100 // 100 (perfect) to 0 (dead)
+      health: 45 // 100 (perfect) to 0 (dead)
     };
   }
 
   componentDidMount() {
     this.timerID = setInterval(
-      () => this.tick(),
+      () => this.tickMood(),
       5000
+    );
+    this.timerHealth = setInterval(
+      () => this.tickHealth(),
+      20000
     );
   }
 
@@ -26,8 +32,23 @@ class Gotchi extends React.Component {
     clearInterval(this.timerID);
   }
 
-  tick() {
+  applyMedicine = () => {
+
+    if(this.state.health < 100){
+      this.setState((state) => ({health: state.health + 10}));
+    }
+    if(this.state.health === 95){
+      this.setState((state) => ({health: 100}))
+    }
+    
+  }
+
+  tickMood() {
     this.setState((state) => ({mood: state.mood - 1}));
+  }
+
+  tickHealth() {
+    this.setState((state) => ({health: state.health - 5}));
   }
 
   selectEmojiFace(){
@@ -44,14 +65,28 @@ class Gotchi extends React.Component {
     return smile;
   }
 
+
   render() {
     return (
       <div>
         <img src={this.selectEmojiFace()} alt="" className="Gotchi-face"/>
         <h6>Age: {this.state.age} | Mood: {this.state.mood} | Health: {this.state.health}</h6>
+        <HealButton applyMedicine={this.applyMedicine}/>
       </div>
+
+    
     );
   }
 }
+class HealButton extends React.Component  {
+  
+    render() {
+      return (
+      <button onClick=
+      {() => this.props.applyMedicine()}>give medicine 💊</button>
+    );
+    }
+    
+  }
 
 export default Gotchi;
