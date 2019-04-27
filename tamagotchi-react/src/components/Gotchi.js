cec7cdbcf70924d3a4dbff635dc11a0ab0737dc6
Anne Penning <apenning@MB018.local>
2019-04-27 12:07:29 +0200

    health stages change emoji face

--- tamagotchi-react/src/components/Gotchi.js
+++ tamagotchi-react/src/components/Gotchi.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import smile from '../emojis/smile.png';
+import sick_puke from '../emojis/sick_puke.png';
+import sick_green from '../emojis/sick_green.png';
+import sick_thermo from '../emojis/sick_thermometer.png';
 import './Gotchi.css';
 
 class Gotchi extends React.Component {
@@ -27,10 +30,24 @@ class Gotchi extends React.Component {
     this.setState((state) => ({mood: state.mood - 1}));
   }
 
+  selectEmojiFace(){
+
+    if(this.state.health <= 30) {
+      return sick_puke;
+    }
+    if(this.state.health <= 50) {
+      return sick_thermo;
+    }
+    if(this.state.health < 60){
+      return sick_green;
+    }
+    return smile;
+  }
+
   render() {
     return (
       <div>
-        <img src={smile} alt="" className="Gotchi-face"/>
+        <img src={this.selectEmojiFace()} alt="" className="Gotchi-face"/>
         <h6>Age: {this.state.age} | Mood: {this.state.mood} | Health: {this.state.health}</h6>
       </div>
     );
