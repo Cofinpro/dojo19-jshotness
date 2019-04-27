326a0ce7e9f7a19ac3921dc513926a2747fb6cd1
Pat <patrick@xplorer.net>
2019-04-27 11:38:54 +0200

    refactoring: extracted Gotchi module to separate files

--- tamagotchi-react/src/App.js
+++ tamagotchi-react/src/App.js
@@ -1,41 +1,6 @@
 import React from 'react';
-import smile from './smile.png';
+import Gotchi from './components/Gotchi.js'
 import './App.css';
-
-class Gotchi extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      age: 0, // starting from 0
-      mood: 0, // range: -5 to 5
-      health: 100 // 100 (perfect) to 0 (dead)
-    };
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      5000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState((state) => ({mood: state.mood - 1}));
-  }
-
-  render() {
-    return (
-      <div>
-        <img src={smile} className="App-logo"/>
-        <h6>Age: {this.state.age} | Mood: {this.state.mood} | Health: {this.state.health}</h6>
-      </div>
-    );
-  }
-}
 
 function App() {
   return ( 
---
+++ tamagotchi-react/src/components/Gotchi.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import smile from '../smile.png';
+import './Gotchi.css';
+
+class Gotchi extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: 0, // starting from 0
+      mood: 0, // range: -5 to 5
+      health: 100 // 100 (perfect) to 0 (dead)
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      5000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState((state) => ({mood: state.mood - 1}));
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={smile} className="Gotchi-face"/>
+        <h6>Age: {this.state.age} | Mood: {this.state.mood} | Health: {this.state.health}</h6>
+      </div>
+    );
+  }
+}
+
+export default Gotchi;
