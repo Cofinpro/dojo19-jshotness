7abdae4231f77178b99a19b07e9c8a0240198d2e
l5s985 <lula.lehmann@cofinpro.de>
2019-04-27 22:03:25 +0200

    Styles

--- tamagotchi/src/app/models/TamagotchiStatus.ts
+++ tamagotchi/src/app/models/TamagotchiStatus.ts
@@ -5,7 +5,7 @@ export class TamagotchiStatus {
   static readonly DANCE = new TamagotchiStatus('Dance', './assets/gifs/lovetenor.gif');
   static readonly PLAY  = new TamagotchiStatus('Play', './assets/gifs/dancetenor.gif');
   static readonly CUDDLE  = new TamagotchiStatus('Cuddle', './assets/gifs/cuddle.gif');
-  static readonly DEAD  = new TamagotchiStatus('Dead', './assets/gifs/end.gif');
+  static readonly DEAD  = new TamagotchiStatus('Dead', './assets/gifs/ezgif.com-crop.gif');
   static readonly CLOSE_TO_DEATH  = new TamagotchiStatus('Close to Death', './assets/gifs/dietenor.gif');
   static readonly LOVE  = new TamagotchiStatus('Love', './assets/gifs/lovetenor.gif');
 
